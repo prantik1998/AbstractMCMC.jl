c140052e7b076660f29dfd40a5dacae7cdf53ef7
prantik1998 <prantikchakraborty50@gmail.com>
2022-04-01 15:02:13 +0530

    transition between states

--- src/sample.jl
+++ src/sample.jl
@@ -226,7 +226,7 @@ function mcmcsample(
             println(states[sampler_id].z.ℓπ.value)
             logα = - (samplers[sampler_id].alg.β - samplers[sampler_id + 1].alg.β) * (states[sampler_id].z.ℓπ.value - states[sampler_id+1].z.ℓπ.value)
             if log(1-Random.rand(rng)) ≤ logα
-                @set samplers[sampler_id].alg.β, samplers[sampler_id + 1].alg.β = samplers[sampler_id + 1].alg.β, samplers[sampler_id].alg.β
+                @set states[sampler_id].z.ℓπ.value, states[sampler_id + 1].z.ℓπ.value = states[sampler_id + 1].z.ℓπ.value,states[sampler_id].z.ℓπ.value
                 accepted_swap_moves[sampler_id] += 1
             else
                 println("Failed swap b/w $sampler_id, $(sampler_id+1)")
